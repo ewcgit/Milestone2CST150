ad51cf1fb316f4a296606eb018575f51f72f8d8b
ewcgit <76733558+ewcgit@users.noreply.github.com>
2021-08-25 16:05:08 -0400

    Add files via upload
    Milestone 2, CST150.

---
+++ Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿
+namespace m2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label7 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 256);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(125, 20);
+            this.label7.TabIndex = 23;
+            this.label7.Text = "Balance ($): 50.00";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button3.Location = new System.Drawing.Point(140, 214);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(84, 29);
+            this.button3.TabIndex = 22;
+            this.button3.Text = "Purchases";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button2.Location = new System.Drawing.Point(63, 214);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(71, 29);
+            this.button2.TabIndex = 21;
+            this.button2.Text = "Restock";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button1.Location = new System.Drawing.Point(12, 214);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(45, 29);
+            this.button1.TabIndex = 20;
+            this.button1.Text = "Buy";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 184);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(136, 20);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "Amount remaining:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 160);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(96, 20);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Product type:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 136);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(122, 20);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Product price ($):";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 20);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Product name:";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(88, 37);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(150, 27);
+            this.numericUpDown2.TabIndex = 15;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(12, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 20);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Quantity:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(88, 4);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(150, 27);
+            this.numericUpDown1.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 20);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "ItemID:";
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button4.Location = new System.Drawing.Point(179, 70);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(56, 29);
+            this.button4.TabIndex = 24;
+            this.button4.Text = "Check";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(247, 284);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace m2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class item
+        {
+            public int itemId { get; set; }
+            public string itemName { get; set; }
+            public double itemPrice { get; set; }
+            public string itemType { get; set; }
+            public int itemQuantityRemaining { get; set; }
+        }
+
+        public List<item> items = new List<item>() {
+            new item{itemId=0, itemName="Sprite", itemPrice=1.99, itemType="Soda", itemQuantityRemaining=10},
+            new item{itemId=1, itemName="Coca-Cola", itemPrice=2.49, itemType="Soda", itemQuantityRemaining=10},
+            new item{itemId=2, itemName="Doritos", itemPrice=3.99, itemType="Chips", itemQuantityRemaining=10},
+            new item{itemId=3, itemName="Lays", itemPrice=4.49, itemType="Chips", itemQuantityRemaining=10},
+            new item{itemId=4, itemName="Pringles", itemPrice=3.29, itemType="Chips", itemQuantityRemaining=10},
+            new item{itemId=5, itemName="Hershey Bar", itemPrice=1.49, itemType="Candy", itemQuantityRemaining=10},
+            new item{itemId=6, itemName="Twizzlers", itemPrice=2.99, itemType="Candy", itemQuantityRemaining=10},
+            new item{itemId=7, itemName="Hubba Bubba", itemPrice=0.99, itemType="Candy", itemQuantityRemaining=10},
+            new item{itemId=8, itemName="Strawberry Nutri-Grain", itemPrice=0.99, itemType="Bar", itemQuantityRemaining=10},
+            new item{itemId=9, itemName="Blueberry Nutri-Grain", itemPrice=0.99, itemType="Bar", itemQuantityRemaining=10},
+            new item{itemId=10, itemName="Apple Nutri-Grain", itemPrice=0.99, itemType="Bar", itemQuantityRemaining=10},
+        };
+
+        public List<item> items2 = new List<item>()
+        {
+        };
+
+        public double balance = 50;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string message = "";
+            string title = "Title";
+
+            if (items[(int)numericUpDown1.Value].itemQuantityRemaining >= (int)numericUpDown2.Value)
+            {
+                if(balance > (items[(int)numericUpDown1.Value].itemPrice * (int)numericUpDown2.Value)){
+                    for (int counter = 0; counter < (int)numericUpDown2.Value; counter++)
+                    {
+                        items2.Add(items[(int)numericUpDown1.Value]);
+                        balance -= items[(int)numericUpDown1.Value].itemPrice;
+                    }
+                    items[(int)numericUpDown1.Value].itemQuantityRemaining -= (int)numericUpDown2.Value;
+                }
+                else
+                {
+                    message = "Balance is too low!";
+                    title = "Warning";
+                    MessageBox.Show(message, title);
+                    message = "";
+                }
+            }
+            else
+            {
+                message = "Stock is too low!";
+                title = "Warning";
+                MessageBox.Show(message, title);
+            }
+
+            for(int counter = 0; counter < items.Count; counter++)
+            {
+                message += "Item ID: " + items[counter].itemId.ToString() +
+                    " | Item Name: " + items[counter].itemName.ToString() +
+                    " | Item Price: $" + items[counter].itemPrice.ToString() +
+                    " | Item Type: " + items[counter].itemType.ToString() +
+                    " | Item Quantity Remaining: " + items[counter].itemQuantityRemaining.ToString() +
+                    "\n";
+            }
+            title = "Inventory";
+            MessageBox.Show(message, title);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int counter = 0; counter < items.Count; counter++)
+            {
+                items[counter].itemQuantityRemaining = 10;
+            }
+            string message = "The stock machine has been refilled!";
+            string title = "Restocked";
+            MessageBox.Show(message, title);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string message = "";
+
+            for (int counter = 0; counter < items2.Count; counter++)
+            {
+                message += "Item ID: " + items2[counter].itemId.ToString() +
+                    " | Item Name: " + items2[counter].itemName.ToString() +
+                    " | Item Price: $" + items2[counter].itemPrice.ToString() +
+                    " | Item Type: " + items2[counter].itemType.ToString() +
+                    "\n";
+            }
+
+            string title = "Title";
+            MessageBox.Show(message, title);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label3.Text = "Product name: " + items[(int)numericUpDown1.Value].itemName.ToString();
+            label4.Text = "Product price ($): " + items[(int)numericUpDown1.Value].itemPrice.ToString();
+            label5.Text = "Product type: " + items[(int)numericUpDown1.Value].itemType.ToString();
+            label6.Text = "Amount remaining: " + items[(int)numericUpDown1.Value].itemQuantityRemaining.ToString();
+            label7.Text = "Balance ($): " + balance.ToString("0.00");
+        }
+    }
+}
